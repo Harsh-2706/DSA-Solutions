ee6245fcdc65e8b0865077065e392dffc5903831
Harsh Bubna <harsh.bubna06@gmail.com>
2026-01-05 09:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (64.60%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        vector<string> rows(numRows);
+        int cur = 0, dir = -1;
+
+        for (char c : s) {
+            rows[cur] += c;
+            if (cur == 0 || cur == numRows - 1) dir *= -1;
+            cur += dir;
+        }
+
+        string ans;
+        for (auto &r : rows) ans += r;
+        return ans;
+    }
+};
