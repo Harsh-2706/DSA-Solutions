41f58ba021c437736e00ac6e69e8e26fbb684636
Harsh Bubna <harsh.bubna06@gmail.com>
2026-01-05 09:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (22.10%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, sign = 1;
+        long ans = 0;
+
+        while (i < s.size() && s[i] == ' ') i++;
+        if (i < s.size() && (s[i] == '-' || s[i] == '+'))
+            sign = s[i++] == '-' ? -1 : 1;
+
+        while (i < s.size() && isdigit(s[i])) {
+            ans = ans * 10 + (s[i++] - '0');
+            if (ans * sign >= INT_MAX) return INT_MAX;
+            if (ans * sign <= INT_MIN) return INT_MIN;
+        }
+        return ans * sign;
+    }
+};
